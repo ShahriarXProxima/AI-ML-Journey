c5eaa45a51ae56f97e6fb57dc652910b8eb0b007
shahriarxproxima <shahriarxproximalog1@gmail.com>
2026-09-25 23:19:37 +0600

    Strings and functions

---
+++ ChapterThree/Functions.py
@@ -0,0 +1,6 @@
+name = "shahriar"
+
+print(len(name))
+print(name.endswith("at")) # true or false
+print(name.startswith("at")) # true or false
+print(name.capitalize())
---
+++ ChapterThree/Strings.py
@@ -0,0 +1,6 @@
+name = "Shahriar"
+
+# string slicing
+nameShort = name[0:4] # start from index 0, all way to index 3 (excluding 4)
+print(nameShort)
+
