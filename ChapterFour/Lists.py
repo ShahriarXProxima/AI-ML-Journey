6c774df98ddf709dd0c9aac3046d44a371c740da
shahriarxproxima <shahriarxproximalog1@gmail.com>
2026-09-26 00:17:43 +0600

    Lists

--- ChapterFour/Lists.py
+++ ChapterFour/Lists.py
@@ -1,5 +1,5 @@
 friends = ["apple", 1, False, 23.234, "mango"]
 print(friends)
 
-friends[1] = "shahriar"  # lists allow assignment
+friends[1] = "shahriar"  # lists allow assignment, lists are mutable
 print(friends)
